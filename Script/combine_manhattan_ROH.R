fe2b4d7988b9dec9adf0712c451e14225b9d954a
Vignesh Arunachalam <128454361+VigneshArunachalam97@users.noreply.github.com>
2024-01-16 11:55:16 +1000

    Create combine_manhattan_ROH.R
    This script combines different ROH manhattan plot into one. Please use the manhattan_roh.R to create a plot for all other population and saved in the only single folder. Use this script to combine the plot. 
    This has a only one argument 
    args[1] - folder with path contains all the RDS object

---
+++ Script/combine_manhattan_ROH.R
@@ -0,0 +1,32 @@
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+### args1 - folder contains the RDS object for other population
+#!/bin/bash
+#### import required packages ####
+library(ggplot2)
+library(cowplot)
+library(dplyr)
+library(stringr)
+
+setwd(args[1]) #set working directory #
+files <- list.files(pattern = ".RDS")
+names <- gsub("_roh_manhattan_obj.RDS", "", 
+              gsub("ukb23352_", "",
+		   gsub("_data", "", files)))       
+names <- str_to_sentence(names)
+names <- gsub("Ni", "Norfolk Islander", 
+              gsub("Pacific", "Pacific Islander", names))
+			  
+plot <- list()
+for(i in 1:length(files)){
+    pl <- readRDS(files[i])
+	pl <- pl + ggtitle(names[i]) + theme(plot.title = element_text(size = 14, face = "bold", color = "Darkblue"), 
+	                                     axis.title.y = element_text(size = 10, face = "bold", color = "black"))
+	plot[[i]] <- pl
+}
+
+final_plot <- plot_grid(plotlist = plot, ncol = 2, nrow = 5, rel_widths = 1, rel_heights = 1)
+
+png("ROH_genomeWide_all_Population.png", w = 3600, h = 1000*5, res = 300, type = "cairo")
+final_plot
+dev.off()
